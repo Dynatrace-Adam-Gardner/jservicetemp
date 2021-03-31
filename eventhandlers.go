edd332b5f29bfc195843a3488b81a69c1b943e7a
Adam Gardner <adam.gardner@dynatrace.com>
2021-03-31 17:50:04 +1000

    Update eventhandlers.go

--- eventhandlers.go
+++ eventhandlers.go
@@ -7,12 +7,39 @@ import (
 	"fmt"
 )
 
+func HandleEvaluationTriggeredEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.EvaluationTriggeredEventData) error {
+  log.Println("Handling evaluation.triggered Event: %s", incomingEvent.Context.GetID())
+  // Got sh.keptn.event.evaluation.triggered
+  // Send a jira-service.started event
+
+  //------------------------------------
+  // 1. Send task started event
+  //------------------------------------
+	_, err := myKeptn.SendTaskStartedEvent(nil, ServiceName)
+
+	if err != nil {
+		errMsg := fmt.Sprintf("Failed to send task started CloudEvent (%s), aborting...", err.Error())
+		log.Println(errMsg)
+		return err
+	}
+
+	return nil
+
+}
+
 func HandleEvaluationFinishedEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudevents.Event, data *keptnv2.EvaluationFinishedEventData) error {
-	log.Printf("Handling evaluation.finished Event: %s", incomingEvent.Context.GetID())
+	log.Println("Handling evaluation.finished Event: %s", incomingEvent.Context.GetID())
+
+	if incomingEvent.Source() == ServiceName {
+		// skip test.finished, it has been sent out by jira-service
+		log.Println("[eventhandlers.go] Received a .finished event from jira-service so stop processing so as to not get into a recursion.")
+		return nil
+	}
 
 	//------------------------------------
 	// 1. Send task started event
 	//------------------------------------
+	/*
 	_, err := myKeptn.SendTaskStartedEvent(data, ServiceName)
 
 	if err != nil {
@@ -20,6 +47,7 @@ func HandleEvaluationFinishedEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudev
 		log.Println(errMsg)
 		return err
 	}
+	*/
 
 	//------------------------------------
 	// 2. Do work
@@ -28,7 +56,7 @@ func HandleEvaluationFinishedEvent(myKeptn *keptnv2.Keptn, incomingEvent cloudev
 	//------------------------------------
 	// 3. Send task finished event
 	//------------------------------------
-	_, err = myKeptn.SendTaskFinishedEvent(data, ServiceName)
+	_, err := myKeptn.SendTaskFinishedEvent(data, ServiceName)
 
 	if err != nil {
 		errMsg := fmt.Sprintf("Failed to send task finished CloudEvent (%s), aborting...", err.Error())
