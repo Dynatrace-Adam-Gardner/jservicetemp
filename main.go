92964a15da1ac8a1cb95521a8dc754ab065dc663
Adam Gardner <adam.gardner@dynatrace.com>
2021-03-31 20:06:12 +1000

    Update main.go

--- main.go
+++ main.go
@@ -7,10 +7,8 @@ package main
 	"log"
 	"os"
 	"strconv"
-
 	cloudevents "github.com/cloudevents/sdk-go/v2" // make sure to use v2 cloudevents here
 	"github.com/kelseyhightower/envconfig"
-
 	keptn "github.com/keptn/go-utils/pkg/lib/keptn"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 )
