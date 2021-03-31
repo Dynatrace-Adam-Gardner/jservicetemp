124239f1d7b66e9f99b075d78f5e705f8ea2b596
Adam Gardner <adam.gardner@dynatrace.com>
2021-03-31 16:10:52 +1000

    Update main.go

--- main.go
+++ main.go
@@ -4,22 +4,21 @@ const ServiceName = "jira-service"
 func processKeptnCloudEvent(ctx context.Context, event cloudevents.Event) error {
   
   // create keptn handler
-	log.Printf("[main.go] Initializing Keptn Handler")
-	myKeptn, err := keptnv2.NewKeptn(&event, keptnOptions)
-	if err != nil {
-		return errors.New("Could not create Keptn Handler: " + err.Error())
-	}
+  log.Printf("[main.go] Initializing Keptn Handler")
+  myKeptn, err := keptnv2.NewKeptn(&event, keptnOptions)
+  if err != nil {
+    return errors.New("Could not create Keptn Handler: " + err.Error())
+  }
   
   switch event.Type() {
     
-	case keptnv2.GetFinishedEventType(keptnv2.EvaluationTaskName):
-		log.Printf("Processing Evaluation.Finished Event")
+  case keptnv2.GetFinishedEventType(keptnv2.EvaluationTaskName):
+    log.Printf("Processing Evaluation.Finished Event")
 
-		eventData := &keptnv2.EvaluationFinishedEventData{}
-		parseKeptnCloudEventPayload(event, eventData)
+    eventData := &keptnv2.EvaluationFinishedEventData{}
+    parseKeptnCloudEventPayload(event, eventData)
 
-		// Handle evaluation.finished and return any errors
-                // See eventhandlers.go
-		return HandleEvaluationFinishedEvent(myKeptn, event, eventData)
-    
+    // Handle evaluation.finished and return any errors
+    // See eventhandlers.go
+    return HandleEvaluationFinishedEvent(myKeptn, event, eventData)
 }
